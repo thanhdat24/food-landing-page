c74d472a2b5712d584d633164c7299d580d6426e
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-06-18 21:22:42 +0700

    Update animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,24 @@
-/*  food category  */
+/* HEADER */
+$(".header__menu li").on("click", function () {
+  $(".header__menu li").removeClass("active");
+  $(this).addClass("active");
+});
 
+/*  food category  */
 $(".food-menu__category button").on("click", function () {
   $(".food-menu__category button").removeClass("active");
   $(this).addClass("active");
 });
 
-
-$(".header__menu li").on("click", function () {
-  $(".header__menu li").removeClass("active");
-  $(this).addClass("active");
+/*Back to top */
+$(".back-to-top").on("click", function () {
+  $("body,html").animate({ scrollTop: 0 }, 500, "linear");
 });
-/* Back to top */
-// $(".back-to-top").on("click", function () {
-//   $("body,html").animate({ scrollTop: 0 }, 1000, "linear");
-// });
+
+let backToTop = document.querySelector(".back-to-top");
+window.onscroll = () => {
+  if (document.body.scrollTop > 300) backToTop.style.display = "block";
+  else {
+    backToTop.style.display = "none";
+  }
+};
