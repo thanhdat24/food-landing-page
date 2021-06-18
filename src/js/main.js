50cb47fd1d30aee8beef07daa6883a75536ed4ae
lethanhdatctu <54407533+thanhdat24@users.noreply.github.com>
2021-06-18 22:13:45 +0700

    update animation

--- src/js/main.js
+++ src/js/main.js
@@ -17,9 +17,18 @@ $(".back-to-top").on("click", function () {
 
 let backToTop = document.querySelector(".back-to-top");
 window.onscroll = () => {
-  if (document.body.scrollTop > 300 ||document.documentElement.scrollTop > 300) {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
     backToTop.style.display = "block";
   } else {
     backToTop.style.display = "none";
   }
 };
+
+$(".food-menu__category button").on("click", function () {
+  $(".food-menu__category button").removeClass("active");
+  $(".food-menu__wrap .animate__animated").removeClass(" animate__fadeInUp");
+  $(this).addClass("active");
+});
